922158f084050d41ffd5045d926df6690516b08a
Phillip Jackson <unisys12@gmail.com>
2012-11-13 06:56:21 -0600

    Tidied up HTML
    Removed alot of the HTML5 Boilerplate stuff in the head. Not sure why I
    used it to begin with. I have a HTML5 snippet. DOH! Added title. Removed
    some empty lines at the end of the file

--- application/views/scroller_view.php
+++ application/views/scroller_view.php
@@ -1,10 +1,13 @@
-<!DOCTYPE html>
-<html lang=en>
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>The Social Scroller</title>
-  <meta name="description" content="Just to see if I could do it">
+  <title></title>
+  <meta name="description" content="">
 
   <meta name="viewport" content="width=device-width">
   <link rel="stylesheet" href="css/style.css">
@@ -43,3 +46,4 @@
 <script src="js/scripts.js"></script>
 </body>
 </html>
+
